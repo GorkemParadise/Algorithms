29645a16ce1e4ce98a7f9b7e54d772a3adcfb858
Görkem Paradise <smailgorkem@gmail.com>
2025-12-08 14:30:00 +0300

    AlgoLeague

---
+++ python/algoleague/usainbolt.py
@@ -0,0 +1,12 @@
+a = int(input().strip())  #rekor sayısı - 5
+b = list(map(int, input().split()))  #rekorlar - 3 7 2 6 8 
+
+high = b[0]
+high_breaks = 0
+
+for i in range(1, a):
+    if b[i] > high:
+        high = b[i]
+        high_breaks += 1
+
+print(high_breaks)  #output - 2
